6f90fae53ed13aff126454f15e3032e19cd67bd8
Jason Piper <j.piper@me.com>
2014-01-29 13:49:22 +0000

    Remove SciPy dependency

--- pyDNase/__init__.py
+++ pyDNase/__init__.py
@@ -13,7 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from _version import __version__
+from . import _version
+__version__ = _version.__version__
 
 import os, math, pysam
 from clint.textui import progress, puts_err
