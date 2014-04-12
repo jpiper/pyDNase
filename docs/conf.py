77cb1ad4d58437560419d33c98ce20f66598b8bc
Jason Piper <j.piper@me.com>
2014-04-12 17:28:15 +0100

    Sexy documentation

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 #
 
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, '../')
 import pyDNase
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -96,8 +97,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
-
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
