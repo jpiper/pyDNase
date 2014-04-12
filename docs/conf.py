c407d0e572228ac12056e64d0f4d9487e3b44ce0
Jason Piper <j.piper@me.com>
2014-04-12 17:34:29 +0100

    sexy document theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 import sys
 sys.path.insert(0, '../')
 import pyDNase
+import sphinx_rtd_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -96,7 +97,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
