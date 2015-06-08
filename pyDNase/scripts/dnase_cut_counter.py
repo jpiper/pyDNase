8231a0228cc108b21c3e70a03f3f278e9fc1535c
Jason Piper <jpiper@humanlongevity.com>
2015-06-08 17:54:28 +0800

    More ATAC compatibility

--- pyDNase/scripts/dnase_cut_counter.py
+++ pyDNase/scripts/dnase_cut_counter.py
@@ -20,12 +20,13 @@ import pyDNase
 from clint.textui import progress, puts
 
 parser = argparse.ArgumentParser(description='Annotates a BED file with with the number of DNase cuts in it')
+parser.add_argument("-A",action="store_true", help="ATAC-seq mode (default: False)",default=False)
 parser.add_argument("regions", help="BED file")
 parser.add_argument("reads", help="The BAM file containing the DNase-seq data")
 parser.add_argument("output", help="filename to write the output to")
 args  = parser.parse_args()
 
-reads  = pyDNase.BAMHandler(args.reads, caching=0)
+reads  = pyDNase.BAMHandler(args.reads, caching=0, ATAC=args.A)
 regions = pyDNase.GenomicIntervalSet(args.regions)
 ofile = open(args.output,"w")
 
